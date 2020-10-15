ccd01748352b489c8d332664fa0cf9ee42bb487b
Conner Chyung <chyungspice@gmail.com>
2020-10-14 18:31:27 -0600

    fix merge conflicts

--- src/components/register/register-layout.js
+++ src/components/register/register-layout.js
@@ -1,16 +1,9 @@
 import React, { useState } from 'react';
 import styled from "styled-components"
 import { Container, Column } from "../../components/grid"
-<<<<<<< HEAD
-import { RegisterContext } from "./register-context"
-import { RegistrationProgress } from "./registration-progress"
-import { FormContainer } from "../form"
-import { useLocation, } from "@reach/router"
-=======
 import { RegistrationProgress } from "./registration-progress"
 import { FormContainer } from "../form"
 import { useLocation, navigate } from "@reach/router"
->>>>>>> 844bac7... redo registration flow
 
 const StyledRegisterLayout = styled.div`
 
@@ -27,16 +20,10 @@ let FormColumn = styled(Column)`
 `
 
 const RegisterLayout = ({ children }) => {
-<<<<<<< HEAD
-    const { href } = useLocation()
-    const [registrationData, setRegistrationData] = useState({})
-
-=======
     const [registrationData, setRegistrationData] = useState({})
 
     const { href } = useLocation()
 
->>>>>>> 844bac7... redo registration flow
     const getCurrentIndex = () => {
         if(href.includes('contact-info')) {
             return 1
@@ -47,23 +34,6 @@ const RegisterLayout = ({ children }) => {
         }
     }
 
-<<<<<<< HEAD
-    const updateRegistrationData = (newFields) => {
-        setRegistrationData({
-            ...registrationData,
-            ...newFields
-        })
-    }
-
-    const registerContext = {
-        registrationData: registrationData,
-        updateRegistrationData: updateRegistrationData
-    }
-
-    console.log(registerContext)
-
-=======
->>>>>>> 844bac7... redo registration flow
     return (
         <StyledRegisterLayout>
             <Container>
@@ -72,21 +42,11 @@ const RegisterLayout = ({ children }) => {
                 </SideBar>
                 <FormColumn>
                     <FormContainer>
-<<<<<<< HEAD
-                        <RegisterContext.Provider value={ registerContext }>
-                            { children }
-                        </RegisterContext.Provider>
-=======
                         { children }
->>>>>>> 844bac7... redo registration flow
                     </FormContainer>
                 </FormColumn>
             </Container>
         </StyledRegisterLayout>
-<<<<<<< HEAD
-        
-=======
->>>>>>> 844bac7... redo registration flow
     )
 }
 
