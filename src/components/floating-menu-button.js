154d94a74338e0557460e4b47e3a46a21c660b23
Chad Helou <chelou@usc.edu>
2021-03-16 10:51:12 -0700

    local fix floating menu

--- src/components/floating-menu-button.js
+++ src/components/floating-menu-button.js
@@ -5,7 +5,7 @@ import HamburgerMenu from 'react-hamburger-menu';
 import { FloatingMenu } from "../components/dashboard/floating-menu"
 
 const Button = styled.div`
-    position: absolute;
+    position: fixed;
     width: 62px;
     height: 62px;
     right: 64px;
