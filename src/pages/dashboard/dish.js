cdd9db66b6faca16cd85438fd7c1e7a3f46552ea
Claire Powers <clairepo@usc.edu>
2021-03-24 16:29:40 -0700

    Fixing image uploading (#63)
    * fixing upload image bug
    
    * add TODO message
    
    * fix(dish.js): add preview of image when editing
    
    * adding replace message

--- src/pages/dashboard/dish.js
+++ src/pages/dashboard/dish.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { navigate } from 'gatsby';
+import styled from "styled-components"
 
 import Client from "../../util/client"
 
@@ -15,6 +16,15 @@ import ModificationForm from '../../components/dashboard/menu-table/form/modifie
 import Navigation from '../../util/navigation';
 
 import { ModificationModal, useModificationModal } from '../../components/dashboard/modal/modification';
+
+const Banner = styled.div`
+  background: url(${({src}) => src});
+  background-size: cover;
+  width: 300px;
+  height: 200px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
 
 const DishPage = ({ location }) => {
     const { state = {} } = location
@@ -36,6 +46,7 @@ const DishPage = ({ location }) => {
 
     const [errorMessage, setErrorMessage] = useState(null)
     const [dishImage, setDishImage] = useState(null)
+    const [selectedDishImageURL, setSelectedDishImageURL] = useState(null);
     const modificationModalControls = useModificationModal();
 
     const updateCategorySelection = (categoryId) => {
@@ -96,6 +107,11 @@ const DishPage = ({ location }) => {
                 delete dish["Modification"];
                 setDishData(dish);
             })
+            Client.getDishImage(dishId).then((res) => {
+                if(res.config.url) {
+                    setSelectedDishImageURL(res.config.url);
+                }
+            })
         }
     }
 
@@ -124,8 +140,14 @@ const DishPage = ({ location }) => {
         try {
             if(create) {
                 await Client.createDish(postDishData);
+                if (dishImage) {
+                    await Client.upsertDishImage(dishId, dishImage);
+                }
             } else {
                 await Client.updateDish(dishId, postDishData);
+                if (dishImage) {
+                    await Client.upsertDishImage(dishId, dishImage);
+                }
             }
 
             navigate('/dashboard/table', { state: { menuId } })
@@ -199,8 +221,11 @@ const DishPage = ({ location }) => {
 
                 <FormSubtitle>Diets</FormSubtitle>
                 <DishDietForm diets={ dishData.Diets } setDiets={ setDishDiets }></DishDietForm> 
-                {/* <FormSubtitle>Image (Optional)</FormSubtitle>
-                <FileDrop acceptedFileTypes={ ['.png', '.jpg', '.jpeg', ] } setFile={ setFile } setErrorMessage={ setErrorMessage } clearFile={ clearFile }/> */}
+                <FormSubtitle>Image (Optional)</FormSubtitle>
+                {
+                    selectedDishImageURL ? <><Banner src={ selectedDishImageURL} /> <div>Replace Image:</div> </> : null
+                }
+                <FileDrop acceptedFileTypes={ ['.png', '.jpg', '.jpeg', ] } setFile={ setFile } setErrorMessage={ setErrorMessage } clearFile={ clearFile }/>
 
                 <FormTitle>Dish Modifiers</FormTitle>
                 <ModificationForm
